18d4685e83e2419b7d574a0c4a0406227e58f2e7
Shameerali <shameerali.in@gmail.com>
2025-02-25 16:44:04 +0530

    Main activity : setup navbar, bottom bar

--- app/src/main/java/com/luminuses/easyshopmvvmcleanarch/ui/MainActivity.kt
+++ app/src/main/java/com/luminuses/easyshopmvvmcleanarch/ui/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.luminuses.easyshopmvvmcleanarch.ui
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.luminuses.easyshopmvvmcleanarch.R
 import com.luminuses.easyshopmvvmcleanarch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +35,80 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupNavigation()
+
     }
+
+    private fun setupNavigation() {
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigation, navController)
+
+        navController.addOnDestinationChangedListener{  _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    updateBottomNavigationVisibility(true)
+                    supportActionBar?.hide()
+                }
+                R.id.cartFragment -> {
+                    updateBottomNavigationVisibility(true)
+                    setActionBarProperties(getString(R.string.title_shopping_cart))
+                }
+
+                else -> {
+                    updateBottomNavigationVisibility(false)
+                    supportActionBar?.hide()
+                }
+            }
+
+        }
+    }
+
+
+    private fun updateBottomNavigationVisibility(isVisible: Boolean) {
+        if (isVisible) {
+            binding.bottomNavigation.visibility = View.VISIBLE
+//            binding.bottomNavigation.visible()
+        } else {
+//            binding.bottomNavigation.setVisibility(View.GONE)
+            binding.bottomNavigation.visibility = View.GONE
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setActionBarProperties(title: String = "") {
+        supportActionBar?.apply {
+            this.title = title
+            this.show()
+            setHomeButtonEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), FLAG)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
+
+    companion object {
+        private const val FLAG = 0
+    }
+
 }
