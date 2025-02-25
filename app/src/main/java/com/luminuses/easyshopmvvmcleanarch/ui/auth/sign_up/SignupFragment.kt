a5f607b20c3418c52414afe67b0a3259f9fa0703
Shameerali <shameerali.in@gmail.com>
2025-02-25 14:27:58 +0530

    #HomeFragment, CartFragment #navGraph update #Login fragment xml design #Signup fragment xml design #Fragment extn, View extn added

--- app/src/main/java/com/luminuses/easyshopmvvmcleanarch/ui/auth/sign_up/SignupFragment.kt
+++ app/src/main/java/com/luminuses/easyshopmvvmcleanarch/ui/auth/sign_up/SignupFragment.kt
@@ -5,56 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.luminuses.easyshopmvvmcleanarch.R
+import com.luminuses.easyshopmvvmcleanarch.databinding.FragmentSignupBinding
+import com.luminuses.easyshopmvvmcleanarch.utils.showToast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+    ): View {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.goToLogin.setOnClickListener(){
+            val action = SignupFragmentDirections.actionSignupFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+
+        binding.btnCreateAccount.setOnClickListener {
+            checkEmptyFields()
+        }
     }
+
+    private fun checkEmptyFields(){
+        val email = binding.email.text.toString()
+        val password = binding.password.text.toString()
+        val name = binding.name.text.toString()
+        val surname = binding.surname.text.toString()
+        val phone = binding.phone.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty() && surname.isNotEmpty()) {
+
+        } else {
+            requireView().showToast(getString(R.string.please_not_blanks))
+        }
+    }
+
 }
---
+++ app/src/main/java/com/luminuses/easyshopmvvmcleanarch/utils/ViewExtensions.kt
@@ -0,0 +1,15 @@
+package com.luminuses.easyshopmvvmcleanarch.utils
+
+import android.view.View
+import android.widget.Toast
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.showToast(text: String) = Toast.makeText(this.context, text, Toast.LENGTH_SHORT).show()
+
